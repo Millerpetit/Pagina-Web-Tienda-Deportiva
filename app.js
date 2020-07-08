94da28035af09575e7c393d92657be8c8e28dbbf
Miller Anderson Petit Rojas <66648476+Millerpetit@users.noreply.github.com>
2020-07-07 23:49:19 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+ const seccionesPagina = new fullpage("#fullpage", {
+    autoScrolling: true, // Se activa el scroll.
+    fitToSection: false, // Acomoda el scroll automaticamente para que la seccion se muestre en pantalla.
+    fitToSectionDelay: 300, // Delay antes de acomodar la seccion automaticamente.
+    easing: 'easeInOutCubic', // Funcion de tiempo de la animacion.
+    scrollingSpeed: 700, // Velocidad del scroll. Valores: en milisegundos.
+    css3: true, // Si usara CSS3 o javascript.
+    easingcss3: 'ease-out', // Curva de velocidad del efecto.
+    loopBottom: false, // Regresa a la primera seccion siempre y cuando se ya haya llegado a la ultima sección y el ususario siga scrolleando.
+
+    navigation: true, // Muesta la barra de navegación.
+	menu: '#menu', // Menu de navegación.
+	anchors: ['Inicio', 'Productos', 'Contactos'], // Anclas, las usamos para identificar cada seccion y poder acceder a ellas con el menu.
+	navigationTooltips: ['Inicio', 'Productos', 'Contactos'], // Tooltips que mostrara por cada boton.
+    showActiveTooltip: false, // Mostrar tooltip activa.
+    
+    sectionsColor : ['#000', '#c2c2c2', '#000'], // Color de fondo de cada seccion.
+    verticalCentered: true, // Si alineara de forma vertical los contenidos de cada seccion.
+    
+    controlArrows: true, // Flechas del slide
+    slidesNavigation: false, // Indicadores del slide
+    
+    afterLoad: function name(origin, destination) {
+        if(destination.anchor == 'Contactos'){
+            document.querySelector('.footer h2').style.opacity = 1;
+        }
+    }
+});
